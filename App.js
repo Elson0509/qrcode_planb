88a17046452e86c53cd354481ae3c70db342643f
Elson Ramos <elson.0509@gmail.com>
2021-07-19 09:04:52 -0400

    adding asyncstorage

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import {NavigationContainer} from '@react-navigation/native'
 import Routes from './src/routes';
 import {AuthProvider} from './src/contexts/auth';
 
+import { StyleSheet,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Image,
+  TouchableOpacity,
+  Text,
+  Animated,
+  Keyboard
+  } from 'react-native';
+
 export default function App() {
   
 
   return (
     <NavigationContainer>
+      {/* <AuthProvider>
+      <View>
+        <Text>ergegre</Text>
+      </View>
+      </AuthProvider> */}
       <AuthProvider>
           <Routes/>
       </AuthProvider>
--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -1,20 +1,51 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+//import AsyncStorage from '@react-native-community/async-storage'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import * as auth from '../services/auth'
+import {View, ActivityIndicator} from 'react-native'
 
 //createContext only set up the format of the variable
 const AuthContext = createContext({ signed: false, user: {}, signIn: ()=>{}, signOut: ()=>{}  })
 
 export const AuthProvider = props =>{
     const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(()=>{
+        const loadStoragegData = async () => {
+            const storagedUser = await AsyncStorage.getItem('@QRSeg:user')
+            const storageToken = await AsyncStorage.getItem('@QRSeg:token')
+            await new Promise(resolve => setTimeout(resolve, 2000))
+            if(storagedUser && storageToken){
+                setUser(JSON.parse(storagedUser))
+                
+            }
+            setLoading(false)
+        }
+        loadStoragegData()
+    }, [])
 
     const signInHandler = async _ => {
         const response = await auth.signIn()
         console.log(response)
         setUser(response.user)
+        await AsyncStorage.setItem('@QRSeg:user', JSON.stringify(response.user))
+        await AsyncStorage.setItem('@QRSeg:token', response.token)
     }
 
     const signOutHandler = async _ => {
-        setUser(null)
+        AsyncStorage.clear().then(()=>{
+            setUser(null)
+        })
+        
+    }
+
+    if(loading){
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <ActivityIndicator size="large" color="#666"/>
+            </View>
+        )
     }
     
     return (
